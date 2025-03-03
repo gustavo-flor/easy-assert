261a89b20e296f3bdb1549adf6d4854e285cb1c7
Gustavo Flôr <ogustaflor@gmail.com>
2025-03-03 08:57:07 -0300

    :beers: Adding CI/CD actions

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "Apache License, Version 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns easy-assert.api})
+  :repl-options {:init-ns easy-assert.api}
+  :repositories [["releases" {:url           "https://clojars.org/repo"
+                              :username      :env/clojars_username
+                              :password      :env/clojars_password
+                              :sign-releases false}]])
