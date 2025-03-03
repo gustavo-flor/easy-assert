65fb57060d023315766a6f8f4f60f6f095758e55
Gustavo Flôr <ogustaflor@gmail.com>
2025-03-03 09:39:26 -0300

    :beers: Adding CI/CD actions

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject com.github.gustavo-flor/easy-assert "0.0.1"
+(defproject com.github.gustavo-flor/easy-assert "0.0.2"
   :description "A Clojure library designed to help you with assertions."
   :url "https://github.com/gustavo-flor/easy-assert"
   :license {:name "Apache License, Version 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
+  :plugins [[lein-project-version "0.1.0"]]
   :repl-options {:init-ns easy-assert.api}
   :repositories [["releases" {:url           "https://clojars.org/repo"
                               :username      :env/clojars_username
